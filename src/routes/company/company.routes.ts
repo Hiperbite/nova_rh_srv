16074f5725aa10ed6670b6bdb5060b8a55b67380
Bruno Mateus <brunofortunatomateus@gmail.com>
2023-08-01 10:21:21 +0100

    company route

---
+++ src/routes/company/company.routes.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import Api from "../../api/Api";
+
+import { Company } from "./../../models/index";
+
+/**
+ * TODO: Find best place to put this stash
+ * @param fn 
+ * @returns 
+ */
+
+const asyncHandler = (fn: any) => (req: any, res: any, next: any) => Promise.resolve(fn(req,res,next)).catch();
+
+const api = new Api(Company);
+const router = express.Router();
+
+router.post("/", asyncHandler(api.create));
+router.put("/:id", asyncHandler(api.update));
+router.delete("/:id", asyncHandler(api.delete));
+router.get("/:id", asyncHandler(api.find));
+router.get("/", asyncHandler(api.findBy));
+
+export default router;
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -4,6 +4,8 @@ import userRoutes from "./common/user.routes";
 import authRoutes from "./common/auth.routes";
 import trackRoutes from "./common/track.routes";
 
+import company from "./company/company.routes";
+
 import dashboards from "./common/dashBoard.routes";
 import helpers from "./common/helper.routes";
 import employees from "./employees/employee.routes";
@@ -18,6 +20,8 @@ routes.use('/tracks', trackRoutes)
 //routes.use('/commons/contacts', contactRoutes)
 routes.use('/commons/dashboards', dashboards)
 routes.use('/commons/helpers', helpers)
+
+routes.use('/company', company)
 
 routes.use('/employees/employees', employees)
 routes.use('/employees/contracts', contracts)
