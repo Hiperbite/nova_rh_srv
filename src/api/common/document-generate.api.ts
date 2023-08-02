b7d7ea7d4f3027824c731f745dc59a908abf929b
Pedro João <pedrojoaodev@gmail.com>
2023-08-02 17:11:08 +0100

    added api service generate document

---
+++ src/api/common/document-generate.api.ts
@@ -0,0 +1,10 @@
+
+import { Response } from "express";
+import generateDocument  from "../../application/common/docs-generate/document.app";
+
+
+export async function getGeneratedDocument(req: Request, res: Response) {
+
+  await generateDocument(res)
+
+}
