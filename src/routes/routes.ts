1a1a6927f426668acbe29b7ff57516a621585aff
Bruno Mateus <brunofortunatomateus@gmail.com>
2023-08-09 09:55:17 +0100

    add business relationship

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -5,6 +5,7 @@ import authRoutes from "./common/auth.routes";
 import trackRoutes from "./common/track.routes";
 
 import company from "./company/company.routes";
+import business from "./company/business.routes";
 
 import dashboards from "./common/dashBoard.routes";
 import helpers from "./common/helper.routes";
@@ -21,7 +22,8 @@ routes.use('/tracks', trackRoutes)
 routes.use('/commons/dashboards', dashboards)
 routes.use('/commons/helpers', helpers)
 
-routes.use('/company', company)
+routes.use('/company/company', company)
+routes.use('/company/business', business)
 
 routes.use('/employees/employees', employees)
 routes.use('/employees/contracts', contracts)
