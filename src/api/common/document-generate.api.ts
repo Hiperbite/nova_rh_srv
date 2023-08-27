205b673c43762ef463cd0812a768d8a91d8eaaa1
Sebastiao Lutonda <lutonda@gmail.com>
2023-08-27 12:36:10 +0100

    fixed doc generator

--- src/api/common/document-generate.api.ts
+++ src/api/common/document-generate.api.ts
@@ -4,7 +4,8 @@ import generateDocument from "../../application/common/docs-generate/document.ap
 
 
 export const getGeneratedDocument = async (req: Request, res: Response) =>
-  (req.query.about) &&
-  await generateDocument({ employeeId: req.params.id, callBack: (e: any) => res.send(e), ...req.query })
+  (req.query.about) 
+  ? await generateDocument({ employeeId: req.params.id, callBack: (e: any) => res.send(e), ...req.query })
+  : res.json();
 
 
