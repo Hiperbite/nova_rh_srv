f6be7c85bef0b035e18c6bf299022ef9715b0a0d
Pedro João <pedrojoaodev@gmail.com>
2023-09-27 12:31:22 +0100

    Fixed attendance justification model

---
+++ src/models/attendance-management/justify.model.ts
@@ -0,0 +1,33 @@
+import {
+    Table,
+    Column,
+    DataType,
+    Scopes,
+    HasMany,
+} from "sequelize-typescript";
+import { Attendance, Model } from "../index";
+
+@Scopes(() => ({
+
+}))
+@Table({
+    timestamps: true,
+    tableName: "AttendanceJustifications",
+})
+export default class AttendanceJustification extends Model {
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: true,
+    })
+    description?: string;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: true
+    })
+    code!: string;
+
+    @HasMany(() => Attendance)
+    attendances!: Attendance[]
+}
