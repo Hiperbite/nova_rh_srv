2e8bae242317de720c2b2fe5ceffb44d08a8d62d
Pedro João <pedrojoaodev@gmail.com>
2023-09-27 12:33:24 +0100

    Attendance routes fixed

---
+++ src/api/attendance-management/attendance.api.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express";
+import { Attendance } from "../../models/index";
+import Api from "../Api";
+
+class AttendanceApi extends Api<Attendance> {
+  constructor() { super(Attendance) };
+
+  create = async (req: Request, res: Response): Promise<Response> => {
+    const { body } = req;
+
+    const justification: Attendance | void = await this.repo.create(body, { include: { all: true } });
+
+    return res.json(justification);
+  };
+
+  update = async (req: Request, res: Response): Promise<Response> => {
+
+    return res.json();
+
+  };
+
+  find = async (req: Request, res: Response): Promise<Response> => {
+    return res.json();
+  }
+}
+
+export default new AttendanceApi();
+export { AttendanceApi };
