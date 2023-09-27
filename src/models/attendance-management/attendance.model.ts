d3b884eb8d1e1027f1d09752e2b53c8e22d12d95
Pedro João <pedrojoaodev@gmail.com>
2023-09-27 12:30:57 +0100

    Attendance model

---
+++ src/models/attendance-management/attendance.model.ts
@@ -0,0 +1,55 @@
+import {
+    Table,
+    Column,
+    DataType,
+    Scopes,
+    BelongsTo,
+    ForeignKey,
+} from "sequelize-typescript";
+import { AttendanceJustification, AttendanceType, Employee, Model } from "../index";
+
+@Scopes(() => ({
+
+}))
+@Table({
+    timestamps: true,
+    tableName: "Attendance",
+})
+export default class Attendance extends Model {
+
+    @Column({
+        type: DataType.DATE,
+        allowNull: true,
+    })
+    startDate?: string;
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: true
+    })
+    endDate!: string;
+
+    @BelongsTo(() => AttendanceType)
+    type!: AttendanceType
+  
+    @ForeignKey(() => AttendanceType)
+    typeId!: string;
+
+    @BelongsTo(() => AttendanceJustification)
+    justifications!: AttendanceJustification[]
+  
+    @ForeignKey(() => AttendanceJustification)
+    justificationId!: string;
+
+    @BelongsTo(() => Employee)
+    employee!: Employee
+  
+    @ForeignKey(() => Employee)
+    employeeId!: string;
+
+    @BelongsTo(() => Employee)
+    approver!: Employee
+  
+    @ForeignKey(() => Employee)
+    approverId!: string;
+}
