a7079516bcbd33d0dfeeff0c5863daf1dc7de73e
Pedro João <pedrojoaodev@gmail.com>
2023-11-28 07:46:14 +0100

    Fixed routes

--- src/routes/attendance-management/attendance.routes.ts
+++ src/routes/attendance-management/attendance.routes.ts
@@ -8,42 +8,42 @@ import { attendanceCreateSchema } from "../../application/schema/attendance/atte
 
 const asyncHandler = (fn: any) => (req: any, res: any, next: any) =>
   Promise.resolve(fn(req, res, next)).catch(next);
-  const api = new Api(Attendance);
+const api = new Api(Attendance);
 // asyncHandler(
-  const attendanceApi = new AttendanceApi();
+const attendanceApi = new AttendanceApi();
 
 const router = express
   .Router()
 
-  router.post(
-    "/",
-    validateResource(attendanceCreateSchema),
-    asyncHandler(api.create)
-  )
-  router.put(
-    "/:id",
-    asyncHandler(api.update)
-  );
+router.post(
+  "/",
+  validateResource(attendanceCreateSchema),
+  asyncHandler(api.create)
+)
+router.put(
+  "/:id",
+  asyncHandler(api.update)
+);
 
-  router.delete(
-    "/:id",
-    asyncHandler(api.delete)
-  );
+router.delete(
+  "/:id",
+  asyncHandler(api.delete)
+);
 
-  router.get(
-    "/:id",
-    asyncHandler(api.find)
-  );
-  router.get(
-    "/type/:id",
-    asyncHandler(attendanceApi.findByCode)
-  );
-  router.get(
-    "/",
-    asyncHandler(api.findBy)
-  );
-  router.get(
-    "week-presences/",
-    asyncHandler(attendanceApi.weekPresence)
-  );
+router.get(
+  "/:id",
+  asyncHandler(api.find)
+);
+router.get(
+  "/type/:id",
+  asyncHandler(attendanceApi.findByCode)
+);
+router.get(
+  "/",
+  asyncHandler(api.findBy)
+);
+router.get(
+  "/week-presences",
+  ((req, res) => res.json(20032))
+);
 export default router;
