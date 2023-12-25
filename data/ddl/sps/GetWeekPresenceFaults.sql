676d6f1ce8f73f5212c3a7d6025860bc64821289
Sebastiao Lutonda <lutonda@gmail.com>
2023-12-25 13:00:52 +0100

    fixer

--- data/ddl/sps/GetWeekPresenceFaults.sql
+++ data/ddl/sps/GetWeekPresenceFaults.sql
@@ -1,20 +1,20 @@
--- Active: 1693908954484@@127.0.0.1@3306@nova_rh
+-- Active: 1667157413070@@127.0.0.1@3306@nova_rh
 
 DELIMITER //
 DROP PROCEDURE GETWEEKPRESENCEFAULTS;
-
-CREATE DEFINER=`root`@`localhost` PROCEDURE `GETWEEKPRESENCEFAULTS`(IN STARTDATE DATE
+-- Active: 1667157413070@@127.0.0.1@3306@nova_rh
+CREATE  PROCEDURE `GETWEEKPRESENCEFAULTS`(IN STARTDATE DATE
 , IN ENDDATE DATE)
 BEGIN 
 	SELECT
 	    COUNT(
 	        IF(
 	            SUBSTRING(
-	                `workinghours`.`weekDays`,
+	                `WorkingHours`.`weekDays`,
 	                1,
 	                2
 	            ) = 'Mo'
-	            and (DAYOFWEEK(attendances.`startDate`) - 1) = 1,
+	            and (DAYOFWEEK(Attendances.`startDate`) - 1) = 1,
 	            1,
 	            NULL
 	        )
@@ -22,11 +22,11 @@ BEGIN
 	    COUNT(
 	        IF(
 	            SUBSTRING(
-	                `workinghours`.`weekDays`,
+	                `WorkingHours`.`weekDays`,
 	                1,
 	                2
 	            ) = 'Tu'
-	            and (DAYOFWEEK(attendances.`startDate`) - 1) = 2,
+	            and (DAYOFWEEK(Attendances.`startDate`) - 1) = 2,
 	            1,
 	            NULL
 	        )
@@ -34,11 +34,11 @@ BEGIN
 	    COUNT(
 	        IF(
 	            SUBSTRING(
-	                `workinghours`.`weekDays`,
+	                `WorkingHours`.`weekDays`,
 	                1,
 	                2
 	            ) = 'We'
-	            and (DAYOFWEEK(attendances.`startDate`) - 1) = 3,
+	            and (DAYOFWEEK(Attendances.`startDate`) - 1) = 3,
 	            1,
 	            NULL
 	        )
@@ -46,11 +46,11 @@ BEGIN
 	    COUNT(
 	        IF(
 	            SUBSTRING(
-	                `workinghours`.`weekDays`,
+	                `WorkingHours`.`weekDays`,
 	                1,
 	                2
 	            ) = 'Th'
-	            and (DAYOFWEEK(attendances.`startDate`) - 1) = 4,
+	            and (DAYOFWEEK(Attendances.`startDate`) - 1) = 4,
 	            1,
 	            NULL
 	        )
@@ -58,11 +58,11 @@ BEGIN
 	    COUNT(
 	        IF(
 	            SUBSTRING(
-	                `workinghours`.`weekDays`,
+	                `WorkingHours`.`weekDays`,
 	                1,
 	                2
 	            ) = 'Fr'
-	            and (DAYOFWEEK(attendances.`startDate`) - 1) = 5,
+	            and (DAYOFWEEK(Attendances.`startDate`) - 1) = 5,
 	            1,
 	            NULL
 	        )
@@ -70,11 +70,11 @@ BEGIN
 	    COUNT(
 	        IF(
 	            SUBSTRING(
-	                `workinghours`.`weekDays`,
+	                `WorkingHours`.`weekDays`,
 	                1,
 	                2
 	            ) = 'Sa'
-	            and (DAYOFWEEK(attendances.`startDate`) - 1) = 6,
+	            and (DAYOFWEEK(Attendances.`startDate`) - 1) = 6,
 	            1,
 	            NULL
 	        )
@@ -82,27 +82,24 @@ BEGIN
 	    COUNT(
 	        IF(
 	            SUBSTRING(
-	                `workinghours`.`weekDays`,
+	                `WorkingHours`.`weekDays`,
 	                1,
 	                2
 	            ) = 'Su'
-	            and (DAYOFWEEK(attendances.`startDate`) - 1) = 0,
+	            and (DAYOFWEEK(Attendances.`startDate`) - 1) = 0,
 	            1,
 	            NULL
 	        )
 	    ) as sundayCount
-	from attendances
-	    LEFT JOIN `employees` on attendances.`employeeId` = employees.id
-	    LEFT JOIN `Contracts` on Contracts.`employeeId` = employees.id
-	    LEFT JOIN workinghours on Contracts.id = `workinghours`.`contractId`
+	from Attendances
+	    LEFT JOIN `Employees` on Attendances.`employeeId` = Employees.id
+	    LEFT JOIN `Contracts` on Contracts.`employeeId` = Employees.id 
+				
+	    LEFT JOIN WorkingHours on Contracts.id = `WorkingHours`.`contractId`
+		LEFT JOIN AttendanceTypes on AttendanceTypes.id=Attendances.typeId 
+				and (AttendanceTypes.code='ABSENT' or AttendanceTypes.code='VACATION')
 	WHERE
-		(attendances.`startDate` BETWEEN STARTDATE and ENDDATE) 
-		and 
-		(employees.`isActive` = 1)
-		and
-	    (attendances.`typeId` = 'f6bcaa60-ee5d-490d-ba0c-d8ca55afa5ca'
-	    OR attendances.`typeId` = '68987a50-8f96-4735-af13-def233140b30');
+		(Attendances.`startDate` BETWEEN STARTDATE and ENDDATE) 
+		and Employees.`isActive` = 1
+		and Contracts.`isActive`  is true;
 	END
-
-
-	//
