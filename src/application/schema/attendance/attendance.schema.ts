23388ebba28c2dc60ef2ddcc3e8da92424db0b5d
Pedro João <pedrojoaodev@gmail.com>
2024-02-10 12:08:46 +0100

    Added frequency create validator

--- src/application/schema/attendance/attendance.schema.ts
+++ src/application/schema/attendance/attendance.schema.ts
@@ -1,15 +1,29 @@
-import {string, object, TypeOf} from "zod";
+import { string, object, TypeOf, number } from "zod";
 
 const infoRequired = "Campo obrigatório";
 
 export const attendanceCreateSchema = object({
     body: object({
-        typeId: string({required_error: infoRequired}).min(1, "typeId inválido"),
-        employeeId: string({required_error: infoRequired}).min(1, "employeeId inválido"),
-        approverId:string({required_error: infoRequired}).min(1, "approverId inválido").optional(),
-        startDate: string({required_error: infoRequired}).min(1, "startDate inválido"),
-        endDate: string({required_error: infoRequired}).min(1, "endDate inválido")
+        typeId: string({ required_error: infoRequired }).min(1, "typeId inválido"),
+        employeeId: string({ required_error: infoRequired }).min(1, "employeeId inválido"),
+        approverId: string({ required_error: infoRequired }).min(1, "approverId inválido").optional(),
+        startDate: string({ required_error: infoRequired }).min(1, "startDate inválido"),
+        endDate: string({ required_error: infoRequired }).min(1, "endDate inválido")
     })
 })
 
 export type attendanceCreateInput = TypeOf<typeof attendanceCreateSchema>["body"];
+
+export const frequencyCreateSchema = object({
+    body: object({
+        state: number({ required_error: infoRequired }),
+        employeeId: string({ required_error: infoRequired }).min(1, "employeeId inválido"),
+        startDate: string({ required_error: infoRequired }).min(1, "startDate inválido"),
+        endDate: string({ required_error: infoRequired }).min(1, "endDate inválido")
+    }).refine((data) => data.endDate >= data.startDate, {
+        message: "Datas inválidas",
+        path: ['Data de fim']
+    })
+})
+
+export type frequencyCreateInput = TypeOf<typeof frequencyCreateSchema>["body"];
